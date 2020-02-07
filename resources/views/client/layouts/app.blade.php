06d4d41a1394a13866e8caa5d78810f1013895c4
deyeo <deyeo@yongsanzip.com>
2020-02-07 12:12:18 +0900

    fix

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -11,6 +11,13 @@
     <link rel="stylesheet" href="{{asset('scss/layout.css')}}">
     <link rel="stylesheet" href="{{asset('scss/client.css')}}">
     <script src="{{asset('js/common.js')}}"></script>
+    <script src="{{asset('js/includeHTML.js')}}"></script>
+    <script src="{{asset('js/portfolioCreate.js')}}"></script>
+    <script src="{{asset('js/project.js')}}"></script>
+    <script src="{{asset('js/projectView.js')}}"></script>
+    <script src="{{asset('js/sponsorView.js')}}"></script>
+    <script src="{{asset('js/swiper.min.js')}}"></script>
+    <script src="{{asset('js/swiper-hero.js')}}"></script>
 
     <title>Figleaf</title>
 </head>
