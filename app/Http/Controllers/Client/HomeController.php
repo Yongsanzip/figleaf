4dbecc4bc8abc2455933eac47641558cf7d9487e
deyeo <deyeo@yongsanzip.com>
2020-02-13 13:48:24 +0900

    modal test

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -13,8 +13,16 @@ class HomeController extends Controller
      * @url : /
      * @return view
      */
-    public function index()
-    {
+    public function index() {
+        error_log("와와아ㅗ아");
+        flash("성공")->success();
+        flash("실패")->error();
+        flash("중요")->important();
+        flash("정보")->info();
+        flash("메세지")->message();
+        flash("경고")->warning();
+        flash( '클리어')->clear();
+        //flash()->overlay('모달창','모달타이틀');
         return view('client.index');
     }
 }
