d5cf8cee624bdb48fc1f694d7a06c4cc79295de0
sykim123 <sykim@yongsanzip.com>
2020-03-09 18:53:05 +0900

    client 1:1 문의하기 생성/삭제/목록 완료

--- resources/views/client/mypage/question/index.blade.php
+++ resources/views/client/mypage/question/index.blade.php
@@ -30,73 +30,48 @@ $tab = 'question';
                         </colgroup>
                         <thead>
                         <tr>
-                            <th>no.</th>
-                            <th>제목</th>
-                            <th>내용</th>
-                            <th>등록일</th>
-                            <th>답변여부</th>
+                            <th style="text-align: center">no.</th>
+                            <th style="text-align: center">제목</th>
+                            <th style="text-align: center">내용</th>
+                            <th style="text-align: center">등록일</th>
+                            <th style="text-align: center">답변여부</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>33</td>
+                        @foreach($datas as $key=>$data)
+                        <tr onclick="location.href='/mypage_question/{{ $data->id }}'">
+                            <td>{{ $datas->total() - ($datas->perPage()* ($datas->currentPage()-1)) - $key }}</td>
                             <td>
                                 <p class="question-title">
-                                    안녕하세요 프로젝트 옵션상품 문의드려요
+                                    {{ $data->title }}
                                 </p>
                             </td>
                             <td>
                                 <p class="question-text">
-                                    봄날의 그들에게 품에 이상의 있는 지혜는 같지 가장 쓸쓸한 황금시대다. 피어나기 품에 꽃 교향악이다. 인도하겠다는 구하지 그와 설레는 보는 속에서 그러므로 위하여서 운다.봄날의 그들에게 품에 이상의 있는 지혜는 같지 가장 쓸쓸한 황금시대다. 피어나기 품에 꽃 교향악이다. 인도하겠다는 구하지 그와 설레는 보는 속에서 그러므로 위하여서 운다.
+                                    {{ $data->content }}
                                 </p>
                             </td>
                             <td>
-                                2020-01-18
+                                {{ $data->created_at->format('Y-m-d') }}
                             </td>
                             <td>
-                                <div class="badge badge-black">yes</div>
-                                <!-- <div class="badge badge-grey">no</div> -->
+                                @if($data->answer_yn == 0)
+                                    <div class="badge badge-grey">no</div>
+                                @else
+                                    <div class="badge badge-black">yes</div>
+                                @endif
                             </td>
                         </tr>
-                        <tr>
-                            <td>33</td>
-                            <td>
-                                <p class="question-title">
-                                    안녕하세요 프로젝트 옵션상품 문의드려요
-                                </p>
-                            </td>
-                            <td>
-                                <p class="question-text">
-                                    봄날의 그들에게 품에 이상의 있는 지혜는 같지 가장 쓸쓸한 황금시대다.
-                                </p>
-                            </td>
-                            <td>
-                                2020-01-18
-                            </td>
-                            <td>
-                                <!-- <div class="badge badge-black">yes</div> -->
-                                <div class="badge badge-grey">no</div>
-                            </td>
-                        </tr>
+                        @endforeach
                         </tbody>
                     </table>
                     <div class="question-row">
-                        <div class="pagination-wrap">
-                            <ul class="pagination">
-                                <li class="prev"><a href="">prev</a></li>
-                                <li class="on"><a href="">1</a></li>
-                                <li><a href="">2</a></li>
-                                <li><a href="">3</a></li>
-                                <li><a href="">4</a></li>
-                                <li><a href="">5</a></li>
-                                <li><a href="">6</a></li>
-                                <li><a href="">7</a></li>
-                                <li><a href="">8</a></li>
-                                <li><a href="">9</a></li>
-                                <li class="next"><a href="">next</a></li>
-                            </ul>
-                        </div>
-                        <a href="" class="btn-white">1:1 문의하기</a>
+                        <nav class="pagination-wrap">
+                            @if($datas->count())
+                                {!! $datas->appends(request()->except('page'))->render() !!}
+                            @endif
+                        </nav>
+                        <a href="/mypage_question/create" class="btn-white">1:1 문의하기</a>
                     </div>
 
 
