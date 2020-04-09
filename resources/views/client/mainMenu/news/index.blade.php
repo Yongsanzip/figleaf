5cc8a9bba3192dca3621d46b8647d0bb28143a7b
sykim123 <sykim@yongsanzip.com>
2020-04-09 13:30:40 +0900

    news 더미 정리

--- resources/views/client/mainMenu/news/index.blade.php
+++ resources/views/client/mainMenu/news/index.blade.php
@@ -19,7 +19,7 @@
 
                 <div class="news-list">
                     <!-- card * 20 -->
-                    <div class="card">
+                    {{--<div class="card">
                         <div class="card-image">
                             <img src="{{asset('/images/dummy/img-dummy-01.png')}}" alt="">
                         </div>
@@ -33,88 +33,13 @@
                             <p class="card-date">2019.08.22</p>
                         </div>
                         <a href="" class="link"></a>
-                    </div>
+                    </div>--}}
                     <!-- //card -->
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="{{asset('/images/dummy/img-dummy-01.png')}}" alt="">
-                        </div>
-                        <div class="card-contents">
-                            <p class="card-title">
-                                천고에 이상의 듣기만 이성은 밝은 그들의 따뜻한 피다. 주며, 살았으며, 얼마나 얼마나 얼마나 거선의 위하여서 이성은 밝은 그들의 따뜻한.
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-name">프레시안</p>
-                            <p class="card-date">2019.08.22</p>
-                        </div>
-                        <a href="" class="link"></a>
-                    </div>
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="{{asset('/images/dummy/img-dummy-01.png')}}" alt="">
-                        </div>
-                        <div class="card-contents">
-                            <p class="card-title">
-                                천고에 이상의 듣기만 이성은 밝은 그들의 따뜻한 피다. 주며, 살았으며, 얼마나 얼마나 얼마나 거선의 위하여서 이성은 밝은 그들의 따뜻한.
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-name">프레시안</p>
-                            <p class="card-date">2019.08.22</p>
-                        </div>
-                        <a href="" class="link"></a>
-                    </div>
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="{{asset('/images/dummy/img-dummy-01.png')}}" alt="">
-                        </div>
-                        <div class="card-contents">
-                            <p class="card-title">
-                                천고에 이상의 듣기만 이성은 밝은 그들의 따뜻한 피다. 주며, 살았으며, 얼마나 얼마나 얼마나 거선의 위하여서 이성은 밝은 그들의 따뜻한.
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-name">프레시안</p>
-                            <p class="card-date">2019.08.22</p>
-                        </div>
-                        <a href="" class="link"></a>
-                    </div>
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="{{asset('/images/dummy/img-dummy-01.png')}}" alt="">
-                        </div>
-                        <div class="card-contents">
-                            <p class="card-title">
-                                거선의 위하여서 이성은 밝은 그들의 따뜻한.
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-name">프레시안</p>
-                            <p class="card-date">2019.08.22</p>
-                        </div>
-                        <a href="" class="link"></a>
-                    </div>
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="{{asset('/images/dummy/img-dummy-01.png')}}" alt="">
-                        </div>
-                        <div class="card-contents">
-                            <p class="card-title">
-                                천고에 이상의 듣기만 이성은 밝은 그들의 따뜻한 피다. 주며, 살았으며, 얼마나 얼마나 얼마나 거선의 위하여서 이성은 밝은 그들의 따뜻한.
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <p class="card-name">프레시안</p>
-                            <p class="card-date">2019.08.22</p>
-                        </div>
-                        <a href="" class="link"></a>
-                    </div>
                 </div>
 
 
                 <div class="btn-wrap">
-                    <div class="btn-more">View More News</div>
+                    {{--<div class="btn-more">View More News</div>--}}
                 </div>
             </section>
             <!--//news -->
