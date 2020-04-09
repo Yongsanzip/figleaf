06547e0018a448efc81a899e09030eb9f08c6536
sykim123 <sykim@yongsanzip.com>
2020-04-09 21:00:20 +0900

    style 추가

--- resources/views/client/project/partial/popup/material.blade.php
+++ resources/views/client/project/partial/popup/material.blade.php
@@ -3,6 +3,48 @@
 <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
 <script src="{{asset('/js/projectAction.js')}}"></script>
 <script src="{{ asset('/js/common.js') }}"></script>
+<style>
+    .text-field {
+        padding: 8px 14px;
+        background: #ecedef;
+        border: none;
+        border-radius: 4px;
+        color: #181818;
+        font-size: 14px;
+        box-sizing: border-box;
+    }
+
+    .btn-white {
+        background: white;
+        border: 1px solid #474747;
+        color: #181818;
+        outline: none;
+        text-align: center;
+        text-transform: uppercase;
+        cursor: pointer;
+        transition: background-color 0.3s;
+        margin-top: 14px;
+        width: 70px;
+        height: 35px;
+    }
+
+    .drop-header {
+        border-bottom: 1px solid #cccccc;
+        font-size: 20px;
+        font-weight: 300;
+        padding-bottom: 4px;
+        padding-left: 10px;
+    }
+    .fabric-title {
+        width: 120px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 16px;
+        font-weight: 700;
+        text-align: center;
+    }
+
+</style>
 <div class="drop-box">
     <div class="drop-header">재질</div>
 
@@ -14,7 +56,7 @@
                 </div>
                 <div class="fabric-item" id="test">
                     <div class="fabric-name">
-                        <select class="select" name="groups" onchange="material(this)">
+                        <select class="select text-field" name="groups" onchange="material(this)">
                             @foreach($groups as $group)
                             <option value="{{ $group->id }}">{{ $group->name_ko }}</option>
                             @endforeach
@@ -24,7 +66,7 @@
                         <div>2차 카테고리</div>
                     </div>
                     <div class="fabric-name" id="material_category">
-                        <select class="select" id="material">
+                        <select class="select text-field" id="material">
                             @foreach($materials as $material)
                             <option value="{{ $material->id }}">{{ $material->name }}</option>
                             @endforeach
@@ -33,7 +75,7 @@
                 </div>
                 <!-- script add item -->
             </div>
-            <button class="btn-black" type="button" onclick="popup_material_close()">완료</button>
+            <button class="btn-white" type="button" onclick="popup_material_close()">완료</button>
         </div>
 
     </div>
