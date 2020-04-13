ff0caa807c1b4eff4f828b5e8032981a068761b2
sykim123 <sykim@yongsanzip.com>
2020-04-13 09:27:11 +0900

    Seeder 삭제

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,7 +15,7 @@ class UserTableSeeder extends Seeder
 
         \App\User::firstOrCreate([
             'role_id' => 4,
-            'email' => 'admin@figleaf.com',
+            'email' => 'admin@figleaf.co.kr',
             'password' => bcrypt('123123'),
             'user_code' => encrypt(date('YmdHmi').\Illuminate\Support\Str::random(10)),
             'name' => '관리자',
@@ -31,50 +31,5 @@ class UserTableSeeder extends Seeder
             'email_verified_at' => \Carbon\Carbon::today(),
             'thumbnail' => '1',
         ]);
-
-        for($i=1;$i<6;$i++){
-            DB::table('users')->insert([
-                'role_id' => 2,
-                'email' => 'design'.$i.'@figleaf.com',
-                'password' => bcrypt('123123'),
-                'user_code' => encrypt(date('YmdHmi').\Illuminate\Support\Str::random(10)),
-                'name' => $faker->name,
-                'home_phone' => '0212341234',
-                'cellphone' => '01012341234',
-                'zip_code' => '11111',
-                'address' => '서울특별시 용산구 문배동 3-3',
-                'address_detail' => '105~108호',
-                'gender' => 0,
-                'grade' => 0,
-                'email_yn' => 1,
-                'sms_yn' => 1,
-                'email_verified_at' => date('Y-m-d H:i:s', time()),
-                'thumbnail' => '1',
-                'created_at' => date('Y-m-d H:i:s', time()),
-                'updated_at' => date('Y-m-d H:i:s', time()),
-            ]);
-        }
-        for($i=1;$i<6;$i++){
-            DB::table('users')->insert([
-                'role_id' => 1,
-                'email' => 'test'.$i.'@figleaf.com',
-                'password' => bcrypt('123123'),
-                'user_code' => encrypt(date('YmdHmi').\Illuminate\Support\Str::random(10)),
-                'name' => $faker->name,
-                'home_phone' => '0212341234',
-                'cellphone' => '01012341234',
-                'zip_code' => '11111',
-                'address' => '서울특별시 용산구 문배동 3-3',
-                'address_detail' => '105~108호',
-                'gender' => 0,
-                'grade' => 0,
-                'email_yn' => 1,
-                'sms_yn' => 1,
-                'email_verified_at' => date('Y-m-d H:i:s', time()),
-                'thumbnail' => '1',
-                'created_at' => date('Y-m-d H:i:s', time()),
-                'updated_at' => date('Y-m-d H:i:s', time()),
-            ]);
-        }
     }
 }
