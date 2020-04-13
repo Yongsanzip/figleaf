faa6b48efeb0e8c9aa5df08e5d9ba5d5cb0d6e2c
sykim123 <sykim@yongsanzip.com>
2020-04-13 09:34:02 +0900

    relation 수정

--- database/migrations/relation/2019_09_23_083329_add_roles_to_users_table.php
+++ database/migrations/relation/2019_09_23_083329_add_roles_to_users_table.php
@@ -14,7 +14,7 @@ class AddRolesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('role_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
