13b46ffef63eb15b8782d70ab585316610a7d405
bobby1815 <bobby1815@nate.com>
2020-04-17 10:11:33 +0900

    fast commit for test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller {
     public function index(){
         try {
             var_dump($_SERVER['REMOTE_ADDR']);
+            var_dump($_SERVER['HTTPS']);
             return view('welcome');
         } catch (\Exception $e){
             $description = '잘못된 접근입니다. <br>'.$e->getMessage();
