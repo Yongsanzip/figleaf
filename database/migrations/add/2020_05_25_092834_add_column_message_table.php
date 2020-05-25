a6ae794a60f342c60956714f91f6040f8c4599df
deyeo <deyeo@yongsanzip.com>
2020-05-25 09:55:40 +0900

    fast fix

---
+++ database/migrations/add/2020_05_25_092834_add_column_message_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned()->comment('사용자 ID : 메세지 등록자 ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn(['user_id']);
+        });
+    }
+}
